55707cbe8839eef96a1a3648751edbb8e185e1ff
Gustavo <guga.rocha.silveira@hotmail.com>
2021-01-18 20:35:20 -0300

    Initial configs backend

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const server = express();
+
+server.use(cors())
+server.use(express.json())
+
+server.get('/', (req, res) => {
+  return res.json({message: 'hello world!'})
+})
+
+server.listen(8080);

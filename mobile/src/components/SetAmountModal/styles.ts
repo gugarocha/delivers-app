7994f3e1759899cfdcf8e349e0317fc65609dee2
Gustavo <guga.rocha.silveira@hotmail.com>
2021-09-01 22:10:47 -0300

    fix SetAmountModal to not break layout with big product names

--- mobile/src/components/SetAmountModal/styles.ts
+++ mobile/src/components/SetAmountModal/styles.ts
@@ -4,19 +4,29 @@ import { theme } from '../../global/styles';
 
 export const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  product: {
+    textAlign: 'center',
+    fontSize: 24,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginTop: 15,
+  },
+  inputLabel: {
+    marginRight: 10,
+    fontSize: 16,
+    color: theme.colors.gray,
+    fontFamily: theme.fonts.light,
+  },
   input: {
     width: 45,
-    marginRight: 10,
     borderBottomWidth: 1,
     borderColor: theme.colors.gray,
     fontSize: 24,
     textAlign: 'center',
-  },
-  product: {
-    fontSize: 24,
   },
 });

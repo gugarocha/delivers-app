eba8ddfff24b8a363c33a78f373c6b98c621a346
Gustavo <guga.rocha.silveira@hotmail.com>
2022-02-12 15:52:02 -0300

    add enviroment variables in backend

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -5,9 +5,9 @@ module.exports = {
   development: {
     client: "postgres",
     connection: {
-      database: "delivers-app",
-      user: "postgres",
-      password: "123456"
+      database: process.env.PG_DATABASE,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD
     },
     migrations: {
       directory: './src/database/migrations'
